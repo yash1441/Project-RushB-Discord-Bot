0bc904d50fe48a10e35756039a47d7e73f968dfa
Simon <yash1441@yahoo.com>
2022-12-26 17:55:48 +0530

    Update index.js
    Add isButton check

--- index.js
+++ index.js
@@ -90,53 +90,55 @@ client.on("interactionCreate", async (interaction) => {
 				content: "There was an error while executing this command!",
 			});
 		}
-	} else if (interaction.customId.startsWith("bug_")) {
-		await interaction.deferReply({ ephemeral: true });
-		let bPhone = interaction.fields.getTextInputValue("bugPhone");
-		let bDetails = interaction.fields.getTextInputValue("bugDetails");
-		let bUserId = interaction.user.id;
-		let bRoleId = interaction.fields.getTextInputValue("bugRole");
-		let bCategory = interaction.customId.substring(4);
+	} else if (interaction.isButton()) {
+		if (interaction.customId.startsWith("bug_")) {
+			await interaction.deferReply({ ephemeral: true });
+			let bPhone = interaction.fields.getTextInputValue("bugPhone");
+			let bDetails = interaction.fields.getTextInputValue("bugDetails");
+			let bUserId = interaction.user.id;
+			let bRoleId = interaction.fields.getTextInputValue("bugRole");
+			let bCategory = interaction.customId.substring(4);
 
-		let file = `${interaction.user.id}-bug.jpg`;
-		let tenantToken = await feishu.authorize(
-			process.env.FEISHU_ID,
-			process.env.FEISHU_SECRET
-		);
-		let response = await feishu.uploadToDrive(
-			tenantToken,
-			process.env.FEEDBACK_BASE,
-			file,
-			"bitable_image"
-		);
-		let file_token = JSON.parse(response).data.file_token;
+			let file = `${interaction.user.id}-bug.jpg`;
+			let tenantToken = await feishu.authorize(
+				process.env.FEISHU_ID,
+				process.env.FEISHU_SECRET
+			);
+			let response = await feishu.uploadToDrive(
+				tenantToken,
+				process.env.FEEDBACK_BASE,
+				file,
+				"bitable_image"
+			);
+			let file_token = JSON.parse(response).data.file_token;
 
-		let bugs = {
-			fields: {
-				"Discord ID": bUserId,
-				"Discord Name": interaction.user.tag,
-				"Role ID": bRoleId,
-				"Bug Details": bDetails,
-				Source: "Discord",
-				"Phone Model": bPhone,
-				"Bug Type": bCategory,
-				Screenshot: [{ file_token: file_token }],
-			},
-		};
+			let bugs = {
+				fields: {
+					"Discord ID": bUserId,
+					"Discord Name": interaction.user.tag,
+					"Role ID": bRoleId,
+					"Bug Details": bDetails,
+					Source: "Discord",
+					"Phone Model": bPhone,
+					"Bug Type": bCategory,
+					Screenshot: [{ file_token: file_token }],
+				},
+			};
 
-		await feishu.createRecord(
-			tenantToken,
-			process.env.FEEDBACK_BASE,
-			process.env.BUG_POOL,
-			bugs,
-			true
-		);
+			await feishu.createRecord(
+				tenantToken,
+				process.env.FEEDBACK_BASE,
+				process.env.BUG_POOL,
+				bugs,
+				true
+			);
 
-		fs.unlinkSync(file);
+			fs.unlinkSync(file);
 
-		await interaction.editReply({
-			content: "Your submission was received successfully!",
-		});
+			await interaction.editReply({
+				content: "Your submission was received successfully!",
+			});
+		}
 	}
 });
 
